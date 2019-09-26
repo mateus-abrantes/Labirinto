399656c4c68882e1fb64fc774e4387c3461d386c
mateus-abrantes <mateus.abrantes7@gmail.com>
2019-09-26 15:17:20 -0300

    Add files

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
+//DEFINIÇÕES DO NUMERO DE COLUNAS E LINHAS, TAMANHO DOS QUADROS(AREA DOS QUADRADOS) DO LABIRINTO E UMA VARIAVEL DE DESCONTO 
+//DE TAMANHO PARA O TAMANHO DO PERSONAGEM EM RELACAO AO LABIRINTO
 var colunas = 41, linhas = 21, tam_quadro = 30, vdesconto = 5;
+//MATRIZ SO TABULEIRO
 var M = [];
+//POSIÇÕES INICIAIS DO JOGADOR (QUADRO DE INICIO)
 var posicao_x_jogador = 45, posicao_y_jogador = 15;
-var x, y;
 
 function setup() {
-    var cnv = createCanvas(colunas * tam_quadro, linhas * tam_quadro);;
-    //var x = (windowWidth - width) / 2;
-    //var y = (windowHeight - height) / 2;
-    //cnv.position(x, y);
+    //CRIAÇÃO DO CANVAS (TELA DO JOGO)
+    createCanvas(colunas * tam_quadro, linhas * tam_quadro);
 
+    //MATRIZ DO LABIRINTO (0 -> PAREDE, 1-> ESPAÇO LIVRE, 2->SAIDA, 3-ENTRADA)
     M = [
         [0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
@@ -34,7 +36,9 @@ function setup() {
 }
 
 function draw() {
+    //COR DE FUNDO DO LABIRINTO (SERVE PARA DESTACAR A COR DO ESPAÇO LIVRE)
     background(161, 161, 161);
+    //CRIAÇÃO DO DESENHO TO LABIRINTO
     for (var i = 0; i < colunas; i++) {
         for (var j = 0; j < linhas; j++) {
             var x = i * tam_quadro;
@@ -42,51 +46,52 @@ function draw() {
             stroke(0);
 
             if (M[j][i] == 3) {
-                //cout << "E";
+                //DESENHA A POSIÇÃO DE ENTRADA "E";
                 fill(216, 227, 0);
                 rect(x, y, tam_quadro, tam_quadro);
             }
             if (M[j][i] == 2) {
-                //cout << "S";
+                //DESENHA A POSIÇÃO DE SAIDA "S"
                 fill(30, 207, 6);
                 rect(x, y, tam_quadro, tam_quadro);
             }
             if (M[j][i] == 0) {
-                // cout << "$";
+                //DESENHA AS PAREDES DO LABIRINTO
                 fill(25);
                 rect(x, y, tam_quadro, tam_quadro);
             }
 
         }
     }
-
+    //COR DO PERSONAGEM E CRIAÇÃO DA ELIPSE COMO PERNSONAGEM
     fill(255, 255, 255);
     ellipse(posicao_x_jogador, posicao_y_jogador, tam_quadro - vdesconto, tam_quadro - vdesconto);
 }
+//FUNCAO QUE FERIFICA QUANDO AS TECLAS SE SETA SAO SOLTAS E MOVE O PERSONAGEM POR UM QUADRADO
 function keyReleased() {
     if ((keyCode === UP_ARROW)) {
-         posicao_y_jogador -= 30;
-         y= -30;
+        posicao_y_jogador -= 30;
+        y = -30;
     }
     if ((keyCode === RIGHT_ARROW)) {
-         posicao_x_jogador += 30;
-         x=30;
+        posicao_x_jogador += 30;
+        x = 30;
     }
     if ((keyCode === LEFT_ARROW)) {
-         posicao_x_jogador -= 30;
+        posicao_x_jogador -= 30;
         x = -30;
     }
     if ((keyCode === DOWN_ARROW)) {
-         posicao_y_jogador += 30;
-         y=30;
+        posicao_y_jogador += 30;
+        y = 30;
     }
-    print("X:"+posicao_x_jogador+" Y:"+posicao_y_jogador);
-    print("M["+(int((posicao_x_jogador+x-15)/30))+"]["+(int((posicao_y_jogador+y)/30))+"]="+M[int((posicao_x_jogador+x)/15)][int((posicao_y_jogador+y)/15)]);
-    //return false; // prevent any default behavior
+    //TESTANDO UMA FORMA DE PROIBIR O PERSONAGEM DE ATRAVESSAR AS PAREDES
+    print("X:" + posicao_x_jogador + " Y:" + posicao_y_jogador);
+    print("M[" + (int((posicao_x_jogador + x - 15) / 30)) + "][" + (int((posicao_y_jogador + y) / 30)) + "]=" + M[int((posicao_x_jogador + x) / 15)][int((posicao_y_jogador + y) / 15)]);
 }
-
-function barreiras(x,y){
-    if(M[int((posicao_x_jogador+x)/15)][int((posicao_y_jogador+y)/15)] != 0 )
+//TESTANDO UMA FORMA DE PROIBIR O PERSONAGEM DE ATRAVESSAR AS PAREDES
+function barreiras(x, y) {
+    if (M[int((posicao_x_jogador + x) / 15)][int((posicao_y_jogador + y) / 15)] != 0)
         return true;
     else return false;
 }
